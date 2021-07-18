b466f135df490f40bec0d9d5b793bfaa066d938c
ABOUBAKAR SIDDIKI <abakostar@gmail.com>
2021-07-18 21:04:09 +0200

    base fonctionnel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponent } from './components/map/map.component';
+import { ScalelineComponent } from './components/scaleline/scaleline.component';
+import { MousePositionComponent } from './components/mouse-position/mouse-position.component';
+import { DecimalPipe } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    ScalelineComponent,
+    MousePositionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DecimalPipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
